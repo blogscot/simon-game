a8e2392af42bdf00c70c4bae88a33fcd5e252aae
iain <iain.diamond@gmail.com>
2017-09-05 19:42:18 +0100

    Brighten panel when user clicks

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Panel from './Panel'
 import ControlPanel from './ControlPanel'
 import PanelColor from '../Enums/PanelColor'
-import Player from '../Audio/Player'
 import PowerState from '../Enums/PowerState'
+import Player from '../Audio/Player'
 
 /**
  * The App component encapsulates the Simon game's
@@ -17,9 +17,17 @@ class App extends Component {
     super()
     this.player = new Player()
 
+    let lightButton = {
+      [PanelColor.Red]: false,
+      [PanelColor.Blue]: false,
+      [PanelColor.Yellow]: false,
+      [PanelColor.Green]: false,
+    }
+
     this.state = {
       powerState: PowerState.Reset,
       strictMode: false,
+      lightButton: lightButton,
     }
   }
   handlePowerSwitch = () => {
@@ -59,6 +67,16 @@ class App extends Component {
   handlePanelClick = color => {
     if (this.state.powerState === PowerState.On) {
       this.player.play(color)
+
+      // light up color panel for a short time
+      let lightButton = { ...this.state.lightButton }
+      lightButton[color] = true
+      this.setState({ lightButton })
+
+      setTimeout(() => {
+        lightButton[color] = false
+        this.setState({ lightButton })
+      }, 300)
     }
   }
   render() {
@@ -67,20 +85,24 @@ class App extends Component {
         <div style={styles.row}>
           <Panel
             style={styles.topLeftPanel}
+            lighten={this.state.lightButton[PanelColor.Green]}
             onClick={() => this.handlePanelClick(PanelColor.Green)}
           />
           <Panel
             style={styles.topRightPanel}
+            lighten={this.state.lightButton[PanelColor.Red]}
             onClick={() => this.handlePanelClick(PanelColor.Red)}
           />
         </div>
         <div style={styles.row}>
           <Panel
             style={styles.bottomLeftPanel}
+            lighten={this.state.lightButton[PanelColor.Yellow]}
             onClick={() => this.handlePanelClick(PanelColor.Yellow)}
           />
           <Panel
             style={styles.bottomRightPanel}
+            lighten={this.state.lightButton[PanelColor.Blue]}
             onClick={() => this.handlePanelClick(PanelColor.Blue)}
           />
         </div>
@@ -99,11 +121,6 @@ class App extends Component {
   }
 }
 
-const basePanel = {
-  borderStyle: 'solid',
-  borderColor: 'black',
-}
-
 const styles = {
   base: {
     marginTop: '20px',
@@ -113,25 +130,21 @@ const styles = {
     justifyContent: 'center',
   },
   topLeftPanel: {
-    ...basePanel,
     borderRadius: '300px 0 0 0',
     background: 'green',
     borderWidth: '16px 8px 8px 16px',
   },
   topRightPanel: {
-    ...basePanel,
     borderRadius: '0 300px 0 0',
     background: '#a50303',
     borderWidth: '16px 16px 8px 8px',
   },
   bottomRightPanel: {
-    ...basePanel,
     borderRadius: '0 0 300px 0',
     background: '#5b75bf',
     borderWidth: '8px 16px 16px 8px',
   },
   bottomLeftPanel: {
-    ...basePanel,
     borderRadius: '0 0 0 300px',
     background: '#c3c32f',
     borderWidth: '8px 8px 16px 16px',
--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Color from 'color'
 
 /**
- * Panel displays a coloured panel, red, blue, yellow or blue.
+ * Panel displays a coloured panel, red, blue, yellow or blue,
+ * which brightens when clicked by the user.
  * 
- * @param {object} style - the component style 
+ * @param {object} style - the component style
+ * @param {bool} lighten - brighten the panel color
+ * @param {function} onClick - click handler  
  * @returns component
  */
 
-const Panel = ({ style, onClick }) => {
-  let styles = Object.assign({}, panelStyles, style)
-  return <div style={styles}
+const Panel = ({ style, lighten, onClick }) => {
+  const lighter = Color(style.background).lighten(0.3)
+  const lighterStyle = lighten ? { background: lighter.string() } : {}
+  const newStyles = Object.assign({}, panelStyles, style, lighterStyle)
+  return <div style={newStyles}
     onClick={onClick} />
 }
 
 const panelStyles = {
   width: '18em',
   height: '18em',
+  borderStyle: 'solid',
+  borderColor: 'black',
 }
 
 Panel.propTypes = {
   style: PropTypes.object.isRequired,
+  lighten: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 }
 
